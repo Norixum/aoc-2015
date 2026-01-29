bd5ce6b4454bb1d2c06888870981d1c97b55fb72
Norixum <norixum@proton.me>
2026-01-30 00:23:29 +0200

    Solution for part 2 of day 12

--- zig/day12/solution.zig
+++ zig/day12/solution.zig
@@ -1,8 +1,8 @@
-const std = @import("std");
-const mem = std.mem;
-const fs = std.fs;
+const std   = @import("std");
 const ascii = std.ascii;
-const fmt = std.fmt;
+const fmt   = std.fmt;
+const fs    = std.fs;
+const mem   = std.mem;
 const print = std.debug.print;
 
 fn readInput(allocator: mem.Allocator) ![]u8 {
@@ -33,65 +33,118 @@ fn part1(input: []const u8) !void {
 
 const Parser = struct {
     content: []const u8,
-    current: ?usize,
+    current: usize,
 
     pub fn new(content: []const u8) Parser {
         return .{
             .content = content,
-            .current = null,
+            .current = 0,
         };
     }
 
-    pub fn nextChar(self: *Parser) ?u8 {
+    pub fn next(self: *Parser) ?u8 {
         if (self.*.current >= self.*.content.len) return null;
+        const result = self.*.content[self.*.current];
         self.*.current += 1;
-        return self.*.content[self.*.current];
-    }
-
-    pub fn currentChar(self: *Parser) u8 {
-        return self.*.content[self.*.current];
-    }
-
-    pub fn trimLeft(self: *Parser) void {
-        while (true) : (self.*.current += 1) {
-            if (self.*.current >= self.*.content.len) break;
-            if (self.*.content[self.*.current] == ' ') continue;
-            self.*.current -= 1;
-            break;
-        }
+        return result;
     }
 };
 
-// fn parseString(content: *[]const u8) !void {
-    
-// }
+fn parseNumber(parser: *Parser) !i64 {
+    const begin = parser.*.current - 1;
+    while (parser.next()) |c| {
+        if (ascii.isDigit(c)) continue;
+        break;
+    }
+    parser.*.current -= 1;
+    const number_str = parser.*.content[begin..parser.*.current];
+    const number = try fmt.parseInt(i64, number_str, 10);
+    return number;
+}
 
-fn parseArray(content: *[]const u8) !void {
-    content.* = mem.trimLeft(u8, content.*, " ");
-    if (content.*[0] == '[') {
-        content.* = content.*[1..];
-        try parseArray(content);
-    } else {
-        print("Unknown char {c}\n", .{content.*[0]});
-        return error.UnknownChar;
+fn parseString(parser: *Parser) []const u8 {
+    const begin = parser.*.current;
+    while (parser.next().? != '"') { }
+    const end = parser.*.current - 1;
+    return parser.*.content[begin..end];
+}
+
+fn parseObject(parser: *Parser) anyerror!i64 {
+    var sum: i64 = 0;
+    var has_red = false;
+    while (true) {
+        var char = parser.next().?;
+
+        if (char == '}') break;
+        std.debug.assert(char == '"');
+        _ = parseString(parser);
+        std.debug.assert(parser.next().? == ':');
+
+        char = parser.next().?;
+        switch (char) {
+            '[' => sum += try parseArray(parser),
+            '"' => {
+                const name = parseString(parser);
+                if (mem.eql(u8, name, "red")) has_red = true;
+            },
+            '{' => sum += try parseObject(parser),
+            '-' => sum += try parseNumber(parser),
+            '0'...'9' => sum += try parseNumber(parser),
+            else => {
+                print("Unknown char {c}\n", .{char});
+                return error.UnknownChar;
+            },
+        }
+
+        char = parser.next().?;
+        if (char == '}') break;
+        std.debug.assert(char == ',');
+    }
+
+    if (has_red) return 0;
+    return sum;
+}
+
+fn parseArray(parser: *Parser) !i64 {
+    var sum: i64 = 0;
+    while (true) {
+        const char = parser.next().?;
+        switch (char) {
+            ']' => return sum,
+            '[' => sum += try parseArray(parser),
+            '"' => _ = parseString(parser),
+            ',' => continue,
+            '{' => sum += try parseObject(parser),
+            '-' => sum += try parseNumber(parser),
+            '0'...'9' => sum += try parseNumber(parser),
+            else => {
+                print("Unknown char {c}\n", .{char});
+                return error.UnknownChar;
+            },
+        }
     }
 }
 
-fn parse(input: []const u8) !void {
+fn parse(input: []const u8) !i64 {
     var parser = Parser.new(input);
-    parser.trimLeft();
-    const char = parser.nextChar().?;
-    if (char == '[') {
-        try parseArray(&parser);
-    } else {
-        print("Unknown char {c}\n", .{char});
-        return error.UnknownChar;
+    const char = parser.next().?;
+    switch (char) {
+        '[' => return try parseArray(&parser),
+        else => {
+            print("Unknown char {c}\n", .{char});
+            return error.UnknownChar;
+        },
     }
+}
+
+fn part2(input: []const u8) !void {
+    const sum = try parse(input);
+    print("Part 2: {}\n", .{sum});
 }
 
 pub fn main() !void {
     const allocator = std.heap.smp_allocator;
     const input = try readInput(allocator);
     try part1(input);
-    try parse(input);
+    try part2(input);
 }
